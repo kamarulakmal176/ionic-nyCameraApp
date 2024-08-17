1e8bf04f734b4f28238caf914362d027849b49f3
Kamarul Akmal <kamarul2893@gmail.com>
2024-08-17 16:23:09 +0800

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  imageUrl: string | undefined;
+
+  constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Get the image as a file URI
+      source: CameraSource.Camera, // Select the camera as the source
+    });
+ 
+    this.imageUrl = image.webPath; // Set the image URL for display
+  }
+
+  async openLibrary() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Get the image as a file URI
+      source: CameraSource.Photos, // Select the photo libary as the source
+    });
+ 
+    this.imageUrl = image.webPath; // Set the image URL for display
+  }
+ 
+ 
+
+}
